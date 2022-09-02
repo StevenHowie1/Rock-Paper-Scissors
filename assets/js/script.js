c20e75c0cfe465f5f1f8e21edbddbacd60fa9b2a
StevenHowie1 <stevenhowie7@hotmail.co.uk>
2022-09-02 15:59:17 +0000

    Add javascript to script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,21 @@
 const pickButtons = document.querySelectorAll('data-pick')
+const select = document.querySelector('[data-select]')
+const computerScoreSpan = document.querySelector('[data-computer-score]')
+const myscoreSpan = document.querySelector('[data-my-score]')
 const PICKS = [
     {
         name: 'rock',
-        img: ''
+        img: '',
         beats: 'scissors'
     },
     {
         name: 'paper',
-        img:
+        img: ,
         beats: 'rock'
     },
     {
         name: 'scissors',
-        img:
+        img: ,
         beats: 'paper'
     }
 ]
@@ -29,6 +32,24 @@ function makepick(pick) {
     const yourWinner = isWinner(pick, computerpick)
     const computerWinner = isWinner(computerpick, pick)
     console.log(computerSelection)
+
+    addSelectionResult(computerpick, computerWinner)
+    addSelectionResult(pick, computerWinner)
+
+    incrementScore(myscoreSpan)
+    incrementScore(computerScoreSpan)
+}
+
+function incrementScore(scoreSpan) {
+    scoreSpan.InnerText = parseInt(scoreSpan.InnerText) + 1
+}
+
+function addSelectionResult(pick, winner) {
+    const div = document.createElement('div')
+    div.innerText = pick.img
+    div.classList.add('selection')
+    if (winner) div.classList.add('winner')
+select.after(div)
 }
 
 function isWinner (pick, opponentpick) {
